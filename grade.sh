5d94f469d55f81d0287052db12ac2424618a11c4
tjune1 <130011651+tjune1@users.noreply.github.com>
2023-05-24 17:24:26 -0700

    Week 6 Lab

--- grade.sh
+++ grade.sh
@@ -8,7 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo "The file ListExamples.java exists in the directory"
+else 
+    echo "The file ListExamples.java doesn't exist in the directory"
+    exit 1
+fi
 
+cp TestListExamples.java grading-area 
+cp student-submission/ListExamples.java grading-area 
+
+javac -cp $CPATH grading-area/*.java
+
+if [[ $? -eq 0 ]]
+then    
+    echo "Compile success!"
+else
+    echo "Compile failure"
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
